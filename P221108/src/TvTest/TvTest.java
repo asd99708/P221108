a64d9fa549dd8c213fa10cf549b13d0a81d69396
Administrator <Administrator@User-2022GUOQCQ>
2022-11-08 15:37:34 +0900

    P221108

--- P221108/src/TvTest/TvTest.java
+++ P221108/src/TvTest/TvTest.java
@@ -1,19 +1,5 @@
 package TvTest;
-class Tv {
-	String color;
-	boolean power;
-	int channel;
-	
-	void power() {
-		power = !power;
-	}
-	void channelUp() {
-		++channel;
-	}
-	void channelDown() {
-		--channel;
-	}
-}
+
 public class TvTest {
 	public static void main(String[] args) {
 		Tv t = new Tv();
